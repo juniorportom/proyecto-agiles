64173f34af125b9a4898365e58e56ea7822ca734
Diego Rodríguez Baquero <diegorbaquero@gmail.com>
2018-09-30 12:16:11 -0500

    Crear modelo de archivo de recurso

---
+++ SGRD/models/archivo.py
@@ -0,0 +1,12 @@
+from django.db import models
+from datetime import datetime
+from .recurso import Recurso
+
+class Archivo(models.Model):
+    nombre = models.CharField(max_length=200)
+    recurso = models.ForeignKey(Recurso, on_delete=models.CASCADE)
+    ruta = models.FileField(upload_to='archivos_recursos/')
+    fecha_creacion = models.DateField(default=datetime.now, blank=True)
+
+    def __str__(self):
+        return self.nombre

d1eac37a510be1746e1be3e07d5be79423f19382
Sebastian Martinez <s.martinezgd@gmail.com>
2018-10-07 08:11:11 -0500

    Arregle un pequeño error en admin.py

--- SGRD/admin.py
+++ SGRD/admin.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from .models.recurso import Recurso, Tipo
 from .models.archivo import Archivo
 from .models.planProduccion import PlanProduccion
+from .models.entradaPlan import EntradaPlan
 # Register your models here.
 admin.site.register(Recurso)
 admin.site.register(Tipo)
 admin.site.register(Archivo)
 admin.site.register(PlanProduccion)
+admin.site.register(EntradaPlan)

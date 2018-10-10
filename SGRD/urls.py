2545fb3ea340351f1dc3fe086f1c44a3157519c6
Diego Rodríguez <diegorbaquero@gmail.com>
2018-10-10 18:21:21 -0500

    Diseño forma adjuntar archivo, arreglo typo, cambio urls

--- SGRD/urls.py
+++ SGRD/urls.py
@@ -4,8 +4,8 @@ from .views import RecursoCreate, ArchivoCreate, RecursoListView
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('create-entrada/', views.createEntradaPlan, name='create-entrada'),
-    path('create-recurso/', RecursoCreate.as_view(), name='create-recurso'),
-    path('create-archivo/', ArchivoCreate.as_view(), name='create-archivo'),
+    path('crear-entrada/', views.createEntradaPlan, name='create-entrada'),
+    path('crear-recurso/', RecursoCreate.as_view(), name='create-recurso'),
+    path('crear-archivo/', ArchivoCreate.as_view(), name='create-archivo'),
     path('recursos/', RecursoListView.as_view(), name='recursos'),
 ]

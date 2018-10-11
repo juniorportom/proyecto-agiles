52aa5859c6687eae1e4d0a405c126eaaa8792a1a
Cristian Martinez <tura182@gmail.com>
2018-10-11 15:27:24 -0500

    Editar plan de produccion

--- SGRD/urls.py
+++ SGRD/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from SGRD import views
-from .views import RecursoCreate, ArchivoCreate, RecursoListView
+from .views import RecursoCreate, ArchivoCreate, RecursoListView, EditarPlanProduccion
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('planProduccion/<int:idRecurso>', views.viewPlanProduccion, name='view-plan'),
+    path('editar-plan-produccion/<int:idPlan>', views.EditarPlanProduccion, name='editar-plan'),
     path('crear-entrada/<int:idRecurso>', views.createEntradaPlan, name='create-entrada'),
     path('crear-recurso/', RecursoCreate.as_view(), name='create-recurso'),
     path('crear-archivo/', ArchivoCreate.as_view(), name='create-archivo'),

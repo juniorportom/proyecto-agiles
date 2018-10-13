7e0e7b8346754fdee7726b8742d5e825a6c5d39e
Reinaldo Portocarrero <juniorportom@gmail.com>
2018-10-13 00:52:35 -0500

    Correccion de excepcion en planes

--- project/settings.py
+++ project/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # Application definition
 
@@ -133,9 +133,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
 
 
 # Media Files
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-# MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'

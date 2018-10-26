d16fb3f12073ae7a25d469bb739fb434b93c1825
Diego Rodríguez <diegorbaquero@gmail.com>
2018-10-25 21:38:51 -0500

    Agregar selenium a dependencias y agregar test básico de editar entrada de plan de producción

---
+++ SGRD/test_editar_entrada_plan.py
@@ -0,0 +1,17 @@
+from unittest import TestCase
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import os
+
+class FunctionalTest(TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+        self.browser.implicitly_wait(2)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_title(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('', self.browser.title)

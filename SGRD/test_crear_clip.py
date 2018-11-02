86b250ba03221ee7f57d40328cab20c7c473fbcb
ALEJANDRA SABOGAL <ja.sabogal@uniandes.edu.co>
2018-11-01 21:19:06 -0500

    Paso 5: Crear un clip con campo tiempo final menor a tiempo inicial (Rojo)

--- SGRD/test_crear_clip.py
+++ SGRD/test_crear_clip.py
@@ -44,9 +44,11 @@ class FunctionalTest(TestCase):
         name.send_keys('Clip 10')
 
         time_start = self.browser.find_element_by_id('id_inicio')
+        time_start.clear()
         time_start.send_keys(10)
 
         time_end = self.browser.find_element_by_id('id_final')
+        time_end.clear()
         time_end.send_keys(20)
 
         self.browser.find_element_by_xpath("//select/option[1]").click()
@@ -88,6 +90,38 @@ class FunctionalTest(TestCase):
 
         li = self.browser.find_element(By.XPATH, '//li[text()="Campo Nombre obligatorio"]')
         self.assertIn('Campo Nombre obligatorio', li.text)
+
+    def test_create_clip_tiempo_final_menor(self):
+        self.browser.get('http://localhost:8000/recursos/')
+        lista = self.browser.find_elements_by_id('id_type')
+        if lista:
+            for item in lista:
+                if item.text == 'Tipo: Video':
+                    parent_element = item.find_element_by_xpath('../../..')
+                    parent_element.find_element_by_link_text('Detalle').click()
+                    break
+        link = self.browser.find_element_by_link_text('Crear clip')
+        self.assertTrue(link.is_displayed())
+        link.click()
+
+        name = self.browser.find_element_by_id('id_nombre')
+        name.send_keys('Clip 10')
+
+        time_start = self.browser.find_element_by_id('id_inicio')
+        time_start.clear()
+        time_start.send_keys(30)
+
+        time_end = self.browser.find_element_by_id('id_final')
+        time_end.clear()
+        time_end.send_keys(20)
+
+        self.browser.find_element_by_xpath("//select/option[1]").click()
+
+        button_create_clip = self.browser.find_element_by_id('create_clip')
+        button_create_clip.click()
+
+        li = self.browser.find_element(By.XPATH, '//li[text()="Tiempo final debe ser mayor al tiempo inicial"]')
+        self.assertIn('Tiempo final debe ser mayor al tiempo inicial', li.text)
 
 
 if __name__ == "__main__":

162a18b6b5c706a1c978d774f5d909481b4a70d7
Sebastian Martinez <s.martinezgd@gmail.com>
2018-11-23 22:55:52 -0500

    Cambio a la prueba de relacionar terminado

--- SGRD/test_relacionar_terminado.py
+++ SGRD/test_relacionar_terminado.py
@@ -1,7 +1,6 @@
 from unittest import TestCase
 from selenium import webdriver
 from django.test import TestCase
-from .models.archivo import Archivo
 from selenium.webdriver.common.by import By
 import os
 
@@ -17,14 +16,6 @@ class FunctionalTest(TestCase):
     def test_title(self):
         self.browser.get('http://localhost:8000/recurso/1')
         self.assertIn('Recursos', self.browser.title)
-
-    def test_string_representation(self):
-        entry = Archivo(descripcion="descripcion de prueba")
-        self.assertEqual(str(entry.descripcion), "descripcion de prueba")
-
-    def test_model_terminado(self):
-        entry = Archivo(terminado=True)
-        self.assertEqual(str(entry.terminado), 'True')
 
     def test_boton_cargar(self):
         self.browser.get('http://localhost:8000/recurso/1')

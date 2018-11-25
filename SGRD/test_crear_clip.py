c273f1fcc22e69b02620297230ab7669b4d3b2e0
Diego Rodríguez <diegorbaquero@gmail.com>
2018-11-25 12:27:53 -0500

    Documentación en forms, models y tests

--- SGRD/test_crear_clip.py
+++ SGRD/test_crear_clip.py
@@ -1,10 +1,13 @@
+# coding=utf-8
 import os
 import unittest
 from unittest import TestCase
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-
+"""
+Tests de la funcionalidad de crear clip de un archivo de video
+"""
 class FunctionalTest(TestCase):
 
     def setUp(self):
@@ -15,6 +18,9 @@ class FunctionalTest(TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    """
+    Test: Existen archivos de tipo video, se puede entrar en detalle y se muestra el botón de crear clip
+    """
     def test_navigation_to_form(self):
         self.browser.get('http://localhost:8000/recursos/')
         lista = self.browser.find_elements_by_id('id_type')
@@ -27,6 +33,9 @@ class FunctionalTest(TestCase):
             link = self.browser.find_element_by_link_text('Crear clip')
             self.assertTrue(link.is_displayed())
 
+    """
+    Test: crear un clip en video
+    """
     def test_create_clip(self):
         self.browser.get('http://localhost:8000/recursos/')
         lista = self.browser.find_elements_by_id('id_type')
@@ -59,6 +68,9 @@ class FunctionalTest(TestCase):
         h5 = self.browser.find_element(By.XPATH, '//h5[text()="Etiquetas:"]')
         self.assertIn('Etiquetas:', h5.text)
 
+    """
+    Test: no se muestra crear clip en recursos que no son videos
+    """
     def test_create_clip_no_video(self):
         self.browser.get('http://localhost:8000/recursos/')
 
@@ -72,6 +84,9 @@ class FunctionalTest(TestCase):
             link_list = self.browser.find_element_by_id('id_links')
             self.assertNotEqual(link_list.text, 'Adjuntar Archivo | Crear plan | Crear clip')
 
+    """
+    Test: No permite creación de clip sin nombre
+    """
     def test_create_clip_vacio(self):
         self.browser.get('http://localhost:8000/recursos/')
         lista = self.browser.find_elements_by_id('id_type')
@@ -91,6 +106,9 @@ class FunctionalTest(TestCase):
             li = self.browser.find_element(By.XPATH, '//li[text()="Campo Nombre obligatorio"]')
             self.assertIn('Campo Nombre obligatorio', li.text)
 
+    """
+    Test: No permite un clip con tiempo final menor al inicial
+    """
     def test_create_clip_tiempo_final_menor(self):
         self.browser.get('http://localhost:8000/recursos/')
         lista = self.browser.find_elements_by_id('id_type')
--- SGRD/test_editar_entrada_plan.py
+++ SGRD/test_editar_entrada_plan.py
@@ -1,8 +1,12 @@
+# coding=utf-8
 from unittest import TestCase
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import os
 
+"""
+Tests de la funcionalidad de editar entradas de plan de producción
+"""
 class FunctionalTest(TestCase):
 
     def setUp(self):
@@ -12,10 +16,16 @@ class FunctionalTest(TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    """
+    Comprobar el título de la página en editar entrada
+    """
     def test_title(self):
         self.browser.get('http://localhost:8000/editar-entrada/1')
         self.assertIn('Editar Entrada', self.browser.title)
 
+    """
+    Se puede editar las personas y guardar
+    """
     def test_edit(self):
         self.browser.get('http://localhost:8000/editar-entrada/1')
         nombre = self.browser.find_element_by_id('id_personas')

5a89efd1f52f47487588081a41ef12947ecd7903
Diego Rodríguez <diegorbaquero@gmail.com>
2018-11-25 11:47:19 -0500

    S3 metadata settings

--- project/settings.py
+++ project/settings.py
@@ -32,6 +32,10 @@ AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_S3_OBJECT_PARAMETERS = {
+    'ContentType': 'application/force-download',
+    'ContentDisposition': 'attachment'
+}
 
 # Application definition
 
